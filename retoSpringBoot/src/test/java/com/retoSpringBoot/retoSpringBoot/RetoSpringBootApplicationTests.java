f52cee9724b86e14e4f3ba03303d57e93b52c96a
Tulio-Rangel <tjrn20@hotmail.com>
2022-08-07 20:51:17 -0500

    configuration spingboot

---
+++ retoSpringBoot/src/test/java/com/retoSpringBoot/retoSpringBoot/RetoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retoSpringBoot.retoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
